898476690ddcb121bae815c4dc7c4cb1a6d9783b
joe <joe@brainleaptech.com>
2020-08-05 16:55:48 -0700

    images from Ara, up to date

--- src/app/teams.ts
+++ src/app/teams.ts
@@ -5,20 +5,24 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Gilead\u00A0Cosman",
-                photo: "assets/images/small/Gili.gif"
+                photo: "assets/images/small/GileadCosman.gif"
             },
             { title: "Proj.\u00A0Manager", name: "Jonathan\u00A0Chaney" },
-            { title: "Artist", name: "Josh\u00A0Bell" },
+            {
+                title: "Artist",
+                name: "Josh\u00A0Bell",
+                photo: "assets/images/small/JoshBell.gif"
+            },
             {
                 title: "Tester",
                 name: "Meagan\u00A0McCarthy",
-                photo: "../../assets/images/small/MeganMcCarthy.gif"
+                photo: "assets/images/small/MeaganMcCarthy.gif"
             },
             { title: "Designer", name: "Luc\u00A0Huynh" },
             {
                 title: "Coach",
                 name: "Brandon\u00A0Ehrlich",
-                photo: "../../assets/images/small/BrandonEhrlich.gif"
+                photo: "assets/images/small/BrandonEhrlich.gif"
             },
         ],
         blurb: "As a more challenging variant of the classic matching card game, Card Quest is a 2D Puzzle game where the player must move cards between two islands to the pattern on each island match."
@@ -29,32 +33,32 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Josh\u00A0Vu",
-                photo: "../../assets/images/small/JoshVu.gif"
+                photo: "assets/images/small/JoshVu.gif"
             },
             {
                 title: "Proj.\u00A0Manager",
                 name: "Joshua\u00A0Gagliano",
-                photo: "../../assets/images/small/JoshuaGagliano.gif"
+                photo: "assets/images/small/JoshuaGagliano.gif"
             },
             {
                 title: "Artist",
                 name: "Elizabeth\u00A0Sweeten",
-                photo: "../../assets/images/small/ElizabethSweeten.gif"
+                photo: "assets/images/small/ElizabethSweeten.gif"
             },
             {
                 title: "Tester",
                 name: "Kanan\u00A0Moran",
-                photo: "../../assets/images/small/KananMoran.gif"
+                photo: "assets/images/small/KananMoran.gif"
             },
             {
                 title: "Designer",
                 name: "Tony\u00A0Rummani",
-                photo: "../../assets/images/small/TonyRummani.gif"
+                photo: "assets/images/small/TonyRummani.gif"
             },
             {
                 title: "Coach",
                 name: "Ara\u00A0Jung",
-                photo: "../../assets/images/small/AraJung.gif"
+                photo: "assets/images/small/AraJung.gif"
             },
         ],
         blurb: "2D side-scroller that navigates the protagonist Plainfin Midshipman across different environments just outside of La Jolla Cove.",
@@ -66,28 +70,28 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Chris\u00A0Welter",
-                photo: "../../assets/images/small/ChrisW.gif"
+                photo: "assets/images/small/ChrisW.gif"
             },
             { title: "Proj.\u00A0Manager", name: "Savannah\u00A0DeYoung" },
             {
                 title: "Artist",
                 name: "Andrew\u00A0Oltman",
-                photo: "../../assets/images/small/AndrewO.gif"
+                photo: "assets/images/small/AndrewO.gif"
             },
             {
                 title: "Tester",
                 name: "James\u00A0Coony",
-                photo: "../../assets/images/small/JamesC.gif"
+                photo: "assets/images/small/JamesCoony.gif"
             },
             { 
                 title: "Designer", 
                 name: "Alyssa\u00A0Gretlein",
-                photo: "../../assets/images/small/Alyssa.gif"
+                photo: "assets/images/small/Alyssa.gif"
             },
             {
                 title: "Coach",
                 name: "Corly\u00A0Huang",
-                photo: "../../assets/images/small/corly_pic.gif"
+                photo: "assets/images/small/CorlyHuang.gif"
             },
         ],
         blurb: "\"Hoyo Negro\" is an underwater exploratory game where players navigate through the Hoyo Negro cave to find bones."
@@ -98,29 +102,33 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Christopher\u00A0Naschke",
-                photo: "../../assets/images/small/ChristopherNaschke.gif"
+                photo: "assets/images/small/ChristopherNaschke.gif"
             },
             {
                 title: "Proj.\u00A0Manager",
                 name: "David\u00A0Frieder",
-                photo: "../../assets/images/small/DavidFrider.gif"
+                photo: "assets/images/small/DavidFrider.gif"
             },
             {
                 title: "Artist",
                 name: "Ian\u00A0Iwinski",
-                photo: "../../assets/images/small/IanIwinski.gif"
+                photo: "assets/images/small/IanIwinski.gif"
             },
             {
                 title: "Tester",
                 name: "Logan\u00A0Wright",
-                photo: "../../assets/images/small/LoganWright.gif"
+                photo: "assets/images/small/LoganWright.gif"
             },
             {
                 title: "Designer",
                 name: "Fiona\u00A0Gibson",
-                photo: "../../assets/images/small/FionaGibson.gif"
+                photo: "assets/images/small/FionaGibson.gif"
             },
-            { title: "Coach", name: "Richard\u00A0Ellks" },
+            {
+                title: "Coach",
+                name: "Richard\u00A0Ellks",
+                photo: "assets/images/small/RichardEllks.gif"
+            },
         ],
         blurb: "2D click-and-drag game based on projectile motion, with the player taking on the role of a chef attempting to toss food at monsters to keep them away through estimating the power and trajectory of the throw.",
         gamelink: "https://drive.google.com/file/d/1byP5mb-UuIh6khitIbw1j6Z9Xyz_yJOA/view"
@@ -131,29 +139,33 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Emma\u00A0Morissette",
-                photo: "../../assets/images/small/Emma.gif"
+                photo: "assets/images/small/Emma.gif"
             },
             {
                 title: "Proj.\u00A0Manager",
                 name: "Connor\u00A0Shinn",
-                photo: "../../assets/images/small/Connor.gif"
+                photo: "assets/images/small/Connor.gif"
             },
-            { title: "Artist", name: "Naseem\u00A0Baramki-Azar" },
+            {
+                title: "Artist",
+                name: "Naseem\u00A0Baramki-Azar",
+                photo: "assets/images/small/Naseem.gif"
+            },
             {
                 title: "Tester",
                 name: "Matthew\u00A0Weaver",
-                photo: "../../assets/images/small/MathewWeaver.gif"
+                photo: "assets/images/small/MathewWeaver.gif"
             },
             {
                 title: "Designer",
                 name: "Seth\u00A0Pairish",
-                photo: "../../assets/images/small/SethPerish.gif"
+                photo: "assets/images/small/SethPerish.gif"
             },
             { 
                 title: "Coach", 
                 name: "Khalil\u00A0Jackson", 
                 linkedin: "https://www.linkedin.com/in/khalil-jackson-0b9ba1112/",
-                photo: "../../assets/images/small/20200803_144714.gif"
+                photo: "assets/images/small/Khalil.gif"
              },
         ],
         blurb: "Based on the classic selective attention and executive control assessment called the Flanker Task, this gamified variant has an old-time cartoon theme."
