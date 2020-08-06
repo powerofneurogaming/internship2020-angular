403400f006321492b843553d11a722280ecd8625
joe <joe@brainleaptech.com>
2020-08-05 17:12:25 -0700

    linkedins added

--- src/app/teams.ts
+++ src/app/teams.ts
@@ -5,27 +5,39 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Gilead\u00A0Cosman",
-                photo: "assets/images/small/GileadCosman.gif"
+                photo: "assets/images/small/GileadCosman.gif",
+                linkedin: "https://www.linkedin.com/in/gilead-c-74ab58aa/"
             },
-            { title: "Proj.\u00A0Manager", name: "Jonathan\u00A0Chaney" },
+            {
+                title: "Proj.\u00A0Manager",
+                name: "Jonathan\u00A0Chaney",
+                linkedin: "https://www.linkedin.com/in/jonathan-chaney/"
+            },
             {
                 title: "Artist",
                 name: "Josh\u00A0Bell",
-                photo: "assets/images/small/JoshBell.gif"
+                photo: "assets/images/small/JoshBell.gif",
+                linkedin: "https://www.linkedin.com/in/andrew-joshua-bell-09b272a7/"
             },
             {
                 title: "Tester",
                 name: "Meagan\u00A0McCarthy",
-                photo: "assets/images/small/MeaganMcCarthy.gif"
+                photo: "assets/images/small/MeaganMcCarthy.gif",
+                linkedin: "https://www.linkedin.com/in/meagan-mccarthy-30392b92/"
             },
-            { title: "Designer", name: "Luc\u00A0Huynh" },
+            {
+                title: "Designer",
+                name: "Luc\u00A0Huynh",
+                linkedin: "https://www.linkedin.com/in/luc-huynh-0520bb1b4/"
+            },
             {
                 title: "Coach",
                 name: "Brandon\u00A0Ehrlich",
                 photo: "assets/images/small/BrandonEhrlich.gif"
             },
         ],
-        blurb: "As a more challenging variant of the classic matching card game, Card Quest is a 2D Puzzle game where the player must move cards between two islands to the pattern on each island match."
+        blurb: "As a more challenging variant of the classic matching card game, Card Quest is a 2D Puzzle game where the player must move cards between two islands to the pattern on each island match.",
+        gameline: "https://dawnbreaker-games.itch.io/matching-card-game"
     },
     {
         teamname: "Journey to the Kelp Forest",
@@ -33,27 +45,32 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Josh\u00A0Vu",
-                photo: "assets/images/small/JoshVu.gif"
+                photo: "assets/images/small/JoshVu.gif",
+                linkedin: "https://www.linkedin.com/in/joshuavu93/"
             },
             {
                 title: "Proj.\u00A0Manager",
                 name: "Joshua\u00A0Gagliano",
-                photo: "assets/images/small/JoshuaGagliano.gif"
+                photo: "assets/images/small/JoshuaGagliano.gif",
+                linkedin: "https://www.linkedin.com/in/joshua-gagliano-84b6501a6/"
             },
             {
                 title: "Artist",
                 name: "Elizabeth\u00A0Sweeten",
-                photo: "assets/images/small/ElizabethSweeten.gif"
+                photo: "assets/images/small/ElizabethSweeten.gif",
+                linkedin: "https://www.linkedin.com/in/elizabeth-sweeten-485935166/"
             },
             {
                 title: "Tester",
                 name: "Kanan\u00A0Moran",
-                photo: "assets/images/small/KananMoran.gif"
+                photo: "assets/images/small/KananMoran.gif",
+                linkedin: "https://www.linkedin.com/in/moran-kanan-378218194/"
             },
             {
                 title: "Designer",
                 name: "Tony\u00A0Rummani",
-                photo: "assets/images/small/TonyRummani.gif"
+                photo: "assets/images/small/TonyRummani.gif",
+                linkedin: "https://www.linkedin.com/in/tony-rummani-12877118a/"
             },
             {
                 title: "Coach",
@@ -70,23 +87,31 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Chris\u00A0Welter",
-                photo: "assets/images/small/ChrisW.gif"
+                photo: "assets/images/small/ChrisW.gif",
+                linkedin: "https://www.linkedin.com/in/christopherwelter/"
             },
-            { title: "Proj.\u00A0Manager", name: "Savannah\u00A0DeYoung" },
+            {
+                title: "Proj.\u00A0Manager",
+                name: "Savannah\u00A0DeYoung",
+                linkedin: "https://www.linkedin.com/in/savannah-deyoung-2533501b3/"
+            },
             {
                 title: "Artist",
                 name: "Andrew\u00A0Oltman",
-                photo: "assets/images/small/AndrewO.gif"
+                photo: "assets/images/small/AndrewO.gif",
+                linkedin: "https://www.linkedin.com/in/andrew-oltman-15bb761b1/"
             },
             {
                 title: "Tester",
                 name: "James\u00A0Coony",
-                photo: "assets/images/small/JamesCoony.gif"
+                photo: "assets/images/small/JamesCoony.gif",
+                linkedin: "https://www.linkedin.com/in/james-coony-657129184/"
             },
             { 
                 title: "Designer", 
                 name: "Alyssa\u00A0Gretlein",
-                photo: "assets/images/small/Alyssa.gif"
+                photo: "assets/images/small/Alyssa.gif",
+                linkedin: "https://www.linkedin.com/in/alyssa-gretlein-8a36581b3/"
             },
             {
                 title: "Coach",
@@ -94,7 +119,8 @@ export const teams = [
                 photo: "assets/images/small/CorlyHuang.gif"
             },
         ],
-        blurb: "\"Hoyo Negro\" is an underwater exploratory game where players navigate through the Hoyo Negro cave to find bones."
+        blurb: "\"Hoyo Negro\" is an underwater exploratory game where players navigate through the Hoyo Negro cave to find bones.",
+        gamelink: "https://developer.cloud.unity3d.com/share/share.html?shareId=bJbJRoHMG8"
     },
     {
         teamname: "Feeding Frenzy",
@@ -102,27 +128,32 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Christopher\u00A0Naschke",
-                photo: "assets/images/small/ChristopherNaschke.gif"
+                photo: "assets/images/small/ChristopherNaschke.gif",
+                linkedin: "https://www.linkedin.com/in/christopher-naschke-0a6219115/"
             },
             {
                 title: "Proj.\u00A0Manager",
                 name: "David\u00A0Frieder",
-                photo: "assets/images/small/DavidFrider.gif"
+                photo: "assets/images/small/DavidFrider.gif",
+                linkedin: "https://www.linkedin.com/in/david-frieder-14662697/"
             },
             {
                 title: "Artist",
                 name: "Ian\u00A0Iwinski",
-                photo: "assets/images/small/IanIwinski.gif"
+                photo: "assets/images/small/IanIwinski.gif",
+                linkedin: "https://www.linkedin.com/in/ian-iwinski-aab0461b1/"
             },
             {
                 title: "Tester",
                 name: "Logan\u00A0Wright",
-                photo: "assets/images/small/LoganWright.gif"
+                photo: "assets/images/small/LoganWright.gif",
+                linkedin: "https://www.linkedin.com/in/logan-wright-989b8015a/"
             },
             {
                 title: "Designer",
                 name: "Fiona\u00A0Gibson",
-                photo: "assets/images/small/FionaGibson.gif"
+                photo: "assets/images/small/FionaGibson.gif",
+                linkedin: "https://www.linkedin.com/in/fiona-gibson-36468618a/"
             },
             {
                 title: "Coach",
@@ -139,17 +170,20 @@ export const teams = [
             {
                 title: "Programmer",
                 name: "Emma\u00A0Morissette",
-                photo: "assets/images/small/Emma.gif"
+                photo: "assets/images/small/Emma.gif",
+                linkedin: "https://www.linkedin.com/in/emma-morissette-553b67189/"
             },
             {
                 title: "Proj.\u00A0Manager",
                 name: "Connor\u00A0Shinn",
-                photo: "assets/images/small/Connor.gif"
+                photo: "assets/images/small/Connor.gif",
+                linkedin: "https://www.linkedin.com/in/connor-shinn-15507880/"
             },
             {
                 title: "Artist",
                 name: "Naseem\u00A0Baramki-Azar",
-                photo: "assets/images/small/Naseem.gif"
+                photo: "assets/images/small/Naseem.gif",
+                linkedin: "https://www.linkedin.com/in/naseem-baramki-azar/"
             },
             {
                 title: "Tester",
@@ -168,6 +202,7 @@ export const teams = [
                 photo: "assets/images/small/Khalil.gif"
              },
         ],
-        blurb: "Based on the classic selective attention and executive control assessment called the Flanker Task, this gamified variant has an old-time cartoon theme."
+        blurb: "Based on the classic selective attention and executive control assessment called the Flanker Task, this gamified variant has an old-time cartoon theme.",
+        gamelink: "https://developer.cloud.unity3d.com/share/share.html?shareId=-JzxOnuGfU"
     },
 ]
